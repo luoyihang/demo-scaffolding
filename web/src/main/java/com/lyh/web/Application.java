955674198d200ca2da7716241d53d979eed242f5
LYH <yhluo03@best-inc.com>
2019-09-18 18:51:40 +0800

    init

--- web/src/main/java/com/lyh/web/Application.java
+++ web/src/main/java/com/lyh/web/Application.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author luo yihang
  * @date 2019/9/18 12:30
  */
-@SpringBootApplication
-@MapperScan(basePackages = "com.lyh.*.dao")
+@SpringBootApplication(scanBasePackages = "com.lyh")
+@MapperScan(basePackages = "com.lyh.**.dao")
 public class Application {
 
     public static void main(String[] args) {
