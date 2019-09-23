494f1287f752975c73ebf183332fbcaf414d8dc6
LYH <yhluo03@best-inc.com>
2019-09-23 18:02:59 +0800

    增加 spring-cloud-sleuth 增加 logback 日志打印

--- dubbo/dubbo-consumer/src/main/java/com/lyh/dubbo/consumer/controller/PersonController.java
+++ dubbo/dubbo-consumer/src/main/java/com/lyh/dubbo/consumer/controller/PersonController.java
@@ -1,6 +1,7 @@
 package com.lyh.dubbo.consumer.controller;
 
 import com.lyh.dubbo.interfaces.PersonDubboService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author luo yihang
  * @date 2019/9/18 19:42
  */
+@Slf4j
 @RestController
 @RequestMapping("/person")
 public class PersonController {
@@ -19,6 +21,8 @@ public class PersonController {
 
     @PostMapping("/getUserName")
     public String getUserName() {
-        return personDubboService.getUserName(123L);
+        String userName = personDubboService.getUserName(123L);
+        log.info(userName);
+        return userName;
     }
 }
