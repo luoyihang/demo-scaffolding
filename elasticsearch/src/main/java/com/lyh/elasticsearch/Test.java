13b5d32bec91d3ddad7564e088f33655ca0f172f
LYH <yhluo03@best-inc.com>
2020-03-17 11:03:14 +0800

    elasticsearch测试

---
+++ elasticsearch/src/main/java/com/lyh/elasticsearch/Test.java
@@ -0,0 +1,33 @@
+package com.lyh.elasticsearch;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+;
+
+
+/**
+ * @author luo yihang
+ * @date 2020/3/4 12:46
+ */
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+//        TransportClient client = new TransportClient() {
+//        };
+//
+//        SearchResponse response = client.prepareSearch("car_shop")
+//                .setTypes("cars")
+//                .setQuery(QueryBuilders.matchQuery("brand", "宝马"))
+//                .get();
+    }
+}
